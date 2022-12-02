d6418df86e12d8d45367f300a01a4dd43ec7683a
lisa <yelizaveta_07@list.ru>
2022-12-02 23:08:43 +0300

    dese 5

--- Program.cs
+++ Program.cs
@@ -1,39 +1,90 @@
-﻿//Задача 25
-Console.WriteLine("Введите число А: ");
-int a = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число Б: ");
-int b = int.Parse(Console.ReadLine()!);
+﻿//Задача 34
+int size = ReadInt("Введите размерность массива: ");
+int [] numbers = new int[size];
 
-int count = a;
-for (int i = 1; i < b; i++)
-{
-    count = count * a;
-}
-Console.WriteLine("Ответ А в степени Б равен: " +count);
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int result = 0;
 
-//Задача 27
-Console.WriteLine("Введите число: ");
-int num = int.Parse(Console.ReadLine()!);
-int GetSize(int number)
+for (int i = 0; i < numbers.Length; i++)
 {
-    int i = 10;
-    int size = 1;
-    while(true)
+    if (numbers[i] % 2 == 0)
     {
-        if(number / i !=0)
-        {
-            size += 1;
-        }
-        else
-        {
-            break;
-        }
-        i *= 10;
+        result++;
     }
-    return(size);
 }
-Console.WriteLine(GetSize(num));
+if (result % 10 == 1)
+{
+    Console.WriteLine($"В массиве {result} четное число");
+}
+if (result % 10 == 2 || result % 10 == 3 || result % 10 == 4)
+{
+    Console.WriteLine($"В массиве {result} четных чисел");
+}
+else
+Console.WriteLine($"В массиве {result} четных чисел");
 
-//Задача 29
-int[] array = GetArray(8);
-Console.WriteLine($"[{String.Join(", ", array) }]");
+//Задача 36
+int size = ReadInt("Введите размерность массива: ");
+int min = ReadInt("Введите минимальное число массива: ");
+int max = ReadInt("Введите максимальное число массива: ");
+int [] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int result = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(i % 2 != 0)
+    {
+        result += numbers[i];
+    }
+}
+Console.WriteLine($"Сумма элементов нечетных позиций = {result}");
+
+//Задача 37
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] PairInArray(int[] array)
+{
+    int 1 = array.Length, size = 1/2;
+    int[]array2;
+    if (1 % 2 == 0)
+    array2 = new int[size];
+    else
+    {
+        array2 = new int[(size +1)];
+        array2[size] = array[size];
+    }
+    for (int i = 0; i < size; i++, 1--)
+    array2[i] = array[1-1];
+    return array2;
+}
+
+//Задача 38
+int size = ReadInt("Введите размер массива: ");
+int min = -10; //ReadInt("Введите минимальное число массива:");
+int max = 10; //ReadInt("Введите максимальное число массива: ");
+int [] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int maxFillarray = numbers[0];
+int minFillarray = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > maxFillarray)
+    {
+        maxFillarray = numbers[i];
+    }
+    if (numbers[i] < minFillarray)
+    {
+        minFillarray = numbers[i];
+    }
+}
+Console.WriteLine($"Разница между максимальным и минимальным числом = {maxFillarra - minFillarray}");
+
